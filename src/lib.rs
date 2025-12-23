addbc11a94f1f203efdec8a6745194dfe5ae3ec1
Tukan van der Borgh <tukanvdborgh@proton.me>
2025-12-24 00:49:22 +0100

    feat: implement 404 response handling and improve request parsing
    Enhance the `handle_connection` function to read and parse incoming HTTP requests, allowing the server to respond with a 404 Not Found status for unrecognized paths.
    Add a new integration test to verify the server's response for non-existent routes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,46 @@
 use anyhow::{Context, Result};
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::net::TcpStream;
 use tracing::instrument;
 
+const RESPONSE_200_OK: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
+const RESPONSE_404_NOT_FOUND: &[u8] = b"HTTP/1.1 404 Not Found\r\n\r\n";
+
 #[instrument(skip(stream))]
 pub fn handle_connection(mut stream: TcpStream) -> Result<()> {
-    const RESPONSE: &str = "HTTP/1.1 200 OK\r\n\r\n";
+    let mut reader = BufReader::new(&stream);
+    let mut buffer = Vec::new();
+
+    loop {
+        reader.read_until(b'\n', &mut buffer)?;
+
+        if buffer.ends_with(b"\r\n\r\n") {
+            break;
+        }
+    }
+
+    let request_str = String::from_utf8_lossy(&buffer);
+    let request = request_str.lines().collect::<Vec<&str>>();
+
+    anyhow::ensure!(!request.is_empty(), "empty request received");
+
+    let first_line = request[0];
+
+    // Example
+    // GET /index.html HTTP/1.1
+    // Hence, we need the second token after splitting via whitespace
+    let path = first_line
+        .split_whitespace()
+        .nth(1)
+        .context("no path in request")?;
+
+    let response = match path {
+        "/" => RESPONSE_200_OK,
+        _ => RESPONSE_404_NOT_FOUND,
+    };
 
     stream
-        .write_all(RESPONSE.as_bytes())
+        .write_all(response)
         .context("failed to write to TcpStream")?;
     stream.flush().context("failed to flush TcpStream")?;
 
