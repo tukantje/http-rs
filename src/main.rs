afa42cc3388bd437d3750298e067dae712af9748
Tukan van der Borgh <tukanvdborgh@proton.me>
2025-12-23 22:18:07 +0100

    feat: implement basic TCP listener
    Add a TCP listener to the main function, allowing the application to accept incoming connections. Log messages for accepted connections and errors encountered during the listening process.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::net::TcpListener;
+
+const LISTENER_URL: &str = "127.0.0.1:42069";
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind(LISTENER_URL).expect("error: failed to create TcpListener instance");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("info: accepted new connection");
+            }
+            Err(error) => {
+                println!("error: {}", error);
+            }
+        }
+    }
 }
